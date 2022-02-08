4ca457c168824767a0e97774d628d5fbbdee9528
OElesin <elesin.olalekan@gmail.com>
2022-02-08 19:39:51 +0100

    Added new updates and frontend

---
+++ apprunner-model-serving/model_loader.py
@@ -0,0 +1,2 @@
+from haystack.nodes import FARMReader
+FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
--- apprunner-model-serving/src/main.py
+++ apprunner-model-serving/src/main.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, Response
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+import pickle
+
+
+class Question(BaseModel):
+    text: str
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Transfer Tracker - AI Powered QnA Bot for Football Transfer News"}
 
 
-@app.get("/hf-summarize")
-async def summarize():
-    return {"message": "Hello World"}
+@app.post("/qna-response/")
+async def qna_response(question: Question):
+    print(question)
+    return question
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def summarize(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
