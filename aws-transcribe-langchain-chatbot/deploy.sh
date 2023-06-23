50697a4da4f744ea5f860e0f4564c193d1161ebb
OElesin <elesin.olalekan@gmail.com>
2023-06-24 00:34:37 +0200

    added sagemaker endpoints to cfn stack

--- aws-transcribe-langchain-chatbot/deploy.sh
+++ aws-transcribe-langchain-chatbot/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
-aws cloudformation deploy --template-file ./cloudformation/template.yaml --stack-name "aws-transcribe-langchain-chatbot" \
+BASE_STACK_NAME="aws-transcribe-langchain-chatbot"
+
+aws cloudformation deploy --template-file ./cloudformation/template.yaml --stack-name "${BASE_STACK_NAME}" \
   --capabilities CAPABILITY_NAMED_IAM \
   --no-fail-on-empty-changeset
